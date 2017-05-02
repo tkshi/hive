38d54ceb3dcaa93e60fd4cc9932c11fd96ed51ac
Takashi Endo <endotakashi1992@gmail.com>
2017-05-03 08:46:51 +0900

    add interface

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from Hatena import Hatena
 from tinydb import TinyDB
 db = TinyDB("db.json")
 
-bots = db.all()
 
-for bot in bots:
-    hatena = Hatena(username=bot['username'],password=bot['password'],proxyserver=bot['proxyserver'])
-    hatena.bookmark(url='https://www.youtube.com/watch?v=c-L8Fm0kke0')
-    hatena.close()
+print "Please type bookmark URL"
+type_url = raw_input()
+print "I will bookmark \n {}.\nOK? y/n".format(type_url)
+type_bool = raw_input()
+if type_bool == 'y':
+    URL = type_url
+    bots = db.all()
+    for bot in bots:
+        hatena = Hatena(username=bot['username'],password=bot['password'],proxyserver=bot['proxyserver'])
+        hatena.bookmark(url=URL)
+        hatena.close()
+else:
+    pass
